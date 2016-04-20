eecdb0df90d800baff180cfd21b9494d9d0bc9f6
Rafał Mikołajun <rafal@mikoweb.pl>
2016-04-20 19:23:15 +0200

    require change

--- lib/liquid-rails-extensions.rb
+++ lib/liquid-rails-extensions.rb
@@ -1,2 +1,2 @@
-require 'liquid'
+require 'liquid4-rails'
 Dir[File.dirname(__FILE__) + '/liquid-rails-extensions/{filters,tags,blocks}/*.rb'].each { |f| require f }
--- lib/liquid-rails-extensions/version.rb
+++ lib/liquid-rails-extensions/version.rb
@@ -1,7 +1,7 @@
 module Liquid
   module Rails
     module Extensions
-      VERSION = '0.0.2'
+      VERSION = '0.0.3'
     end
   end
 end
